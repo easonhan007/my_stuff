27747531a0f7f2d7cc1c95b545150441e8168d54
Yichun Han <yichun.han@shopee.com>
2023-06-25 15:51:10 +0800

    do not force ssl on production

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-%w(U盘 数据线 手柄 主机). each do |c_name|
-	puts "创建 #{c_name}"
-	Category.find_or_create_by!(name: c_name)
+if Rails.env != 'production'
+	%w(U盘 数据线 手柄 主机). each do |c_name|
+		puts "创建 #{c_name}"
+		Category.find_or_create_by!(name: c_name)
+	end
 end
